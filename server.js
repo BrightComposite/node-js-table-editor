60f8632b2c25629ba66d38e975fd80e87ebbfa99
BrightComposite <maestro.aquarius@gmail.com>
2016-08-31 21:19:40 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.use(express.logger());
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
   res.sendFile('/index.html')
 })
 
 var port = process.env.PORT || 5000;
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-})
+app.listen(port)
 
 console.log("Server started!")
